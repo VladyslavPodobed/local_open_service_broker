161d72ac6e2fe840cf6d8f46f4064e8376cc5d3e
VladyslavPodobed <vladyslav.podobed@gmail.com>
2026-07-09 22:59:54 +0000

    renamed some variables following best principles

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import re
+import subprocess
 from typing import Any
 
 
 class UserInput:
     def __init__(self) -> None:
-        self.dictionary_with_mappings: dict[str, int] = {
+        self.service_to_number_mappings: dict[str, int] = {
             "postgresql": 1,
             "mysql": 2,
             "redis": 3,
@@ -21,13 +22,13 @@ class UserInput:
             if u need PostgreSQL - $: 1
             if u need PostgreSQL and MySQL - $: 1 2
         """
-        self.dict_with_config_questions: dict[str, bool] = {
+        self.config_questions: dict[str, bool] = {
             # True is for inputs that must be int - done by the convert_str_to_int function and checked by the convert_str_to_int fucntion
             "Input port that should be open on the host: ": True,
             "Input port that should be open on the container: ": True, 
             "Input db's password: ": False
         }
-        self.dict_with_selected_services_and_config: dict[str, dict[str, Any]] = {}
+        self.selected_services_and_config: dict[str, dict[str, Any]] = {}
 
 
     # the function is used in user_selects_services; it exists so that I can jsut add any number of services to the "offered_services" var
@@ -61,10 +62,10 @@ class UserInput:
         return selected_services_list
 
 
-    def determine_selected_services(self, user_response: list[int], dictionary_with_mappings: dict[str, int]) -> list[str]:
-        for key_in_dict in dictionary_with_mappings:
-            for int_in_set in user_response:
-                if int_in_set == dictionary_with_mappings.get(key_in_dict):
+    def determine_selected_services(self, user_response: list[int], service_to_number_mappings: dict[str, int]) -> list[str]:
+        for key_in_dict in service_to_number_mappings:
+            for selected_service_number in user_response:
+                if selected_service_number == service_to_number_mappings.get(key_in_dict):
                     print(f"determine_selected_services returned: --- {key_in_dict}")
                     self.selected_services.append(key_in_dict)
 
@@ -81,33 +82,48 @@ class UserInput:
                 string_being_converted = input(question_being_asked)
 
 
-    def prompt_for_service_config(self, dict_of_config_question: dict[str, bool]) -> dict[str, Any]:
-        dict_with_config: dict[str, Any] = {
+    def prompt_for_service_config(self, config_questions: dict[str, bool]) -> dict[str, Any]:
+        config: dict[str, Any] = {
             "HOST_PORT": "",
             "CONTAINER_PORT": "",
             "DB_PASSWORD": ""
         }
-        for (question, key_in_final_dict) in zip(dict_of_config_question, dict_with_config):
+        for (question, key_in_final_dict) in zip(config_questions, config):
             print(question)
             prompt_to_input_config = input("")
-            if bool(dict_of_config_question[question]):
+            if bool(config_questions[question]):
                 prompt_to_input_config = self.convert_str_to_int(prompt_to_input_config, question)
-            dict_with_config[key_in_final_dict] = prompt_to_input_config
+            config[key_in_final_dict] = prompt_to_input_config
 
-        return dict_with_config
+        return config
 
 
     def map_services_to_configs(self, services: list[str]) -> dict[str, dict[str, Any]]:
         for each_service in services:
-            self.dict_with_selected_services_and_config[each_service] = self.prompt_for_service_config(self.dict_with_config_questions)
+            self.selected_services_and_config[each_service] = self.prompt_for_service_config(self.config_questions)
 
-        return self.dict_with_selected_services_and_config
+        return self.selected_services_and_config
 
 
     def main(self):
         print(self.offered_services)
-        selected_services = self.determine_selected_services(self.user_selects_services(), self.dictionary_with_mappings)
+        selected_services = self.determine_selected_services(self.user_selects_services(), self.service_to_number_mappings)
         self.map_services_to_configs(selected_services)
+
+
+class FillOutConfigFile:
+    def __init__(self) -> None:
+        self.config_dir_path = "./generated/"
+
+
+    def convert_dict_to_yaml(self, services_config: dict[str, dict[str, Any]]):
+     return
+
+
+    # def input_config_into_file(self, passed_config):
+    #     with open(self.config_dir_path, "w"):
+    #         pass
+        # subprocess.run()
 
 
 if __name__ == "__main__":
