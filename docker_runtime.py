dcc093edd5045d315af9284298aa1edcd446d974
VladyslavPodobed <vladyslav.podobed@gmail.com>
2026-08-29 12:54:23 +0000

    refactored are_all_containers_up method to use list comprehension

--- docker_runtime.py
+++ docker_runtime.py
@@ -15,32 +15,26 @@ class DockerRuntime(ContainerRuntime):
         if docker_compose_up.returncode != 0:
             print(f"couldn't spin up containers due to: {docker_compose_up.stderr}")
             return Failure(error_description = f"couldn't spin up containers due to: {docker_compose_up.stderr}")
-        else:
-            print('container/s are up')
+        print('container/s are up')
         return Success(return_value = docker_compose_up.returncode)
 
     def inspect_containers(self) -> Success[dict] | Failure:
         running_containers_ids: list = docker_client.containers.list(all = True, sparse=True)
         containers_config: dict[str, Any] = {}
         for container in running_containers_ids:
-            container_attrs = container.attrs
-            containers_config[container_attrs["Names"][0].lstrip('/')] = container_attrs
-        return Success(return_value=containers_config)
+            # removed redundant code here
+            # var was literally useless = container_attrs = container.attrs
+            containers_config[container.attrs["Names"][0].lstrip('/')] = container.attrs
+        return Success(return_value = containers_config)
 
-    def are_all_containers_up(self, selected_services: list[str], running_services: dict[str, Any]) -> Success | Failure:
-        not_spun_up_services: list[str] = []
-        running_containers = running_services.keys()
-        for service in running_containers:
-            if service not in selected_services:
-                not_spun_up_services.append(service)
+    def are_all_containers_up(self, selected_services, running_containers_names) -> Success | Failure:
+        not_spun_up_services = [service for service in selected_services if service not in running_containers_names]
         if not not_spun_up_services:
-            print(f"containers weren't created for the services: {not_spun_up_services}")
-            return Failure(error_description = f"containers weren't created for the services: {not_spun_up_services}")
-        return Success(return_value = "")
+            return Success(return_value=True)
+        return Failure(error_description=f"containers weren't created for the following services: {not_spun_up_services}")
+
 
     def are_all_containers_healthy(self, ):
         # check "State"
         pass
         
-
-
