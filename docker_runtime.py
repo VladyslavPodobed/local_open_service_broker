439808e5663ed508a6f0ff1f1bd62be017c7e1e3
VladyslavPodobed <vladyslav.podobed@gmail.com>
2026-09-06 22:38:32 +0000

    got rid of dataclass for method return type hinting and added verify_dependency method

--- docker_runtime.py
+++ docker_runtime.py
@@ -1,7 +1,6 @@
 import subprocess
 import docker
 from typing import Any
-from models import Success, Failure
 from container_runtime import ContainerRuntime
 
 
@@ -10,38 +9,49 @@ docker_client = docker.from_env()
 
 class DockerRuntime(ContainerRuntime):
 
-    def spin_up_containers(self) -> Success[int] | Failure:
-        docker_compose_up = subprocess.run(['docker', 'compose', '-f', './generated/docker-compose.yml', 'up', '-d'], capture_output=True)
+    @staticmethod
+    def verify_dependency() -> int:
+        try:
+            docker_compose_dependency = subprocess.run(['docker', 'compose', 'version'], capture_output=True)
+        except FileNotFoundError as e:
+            raise SystemExit(f"missing docker compose dependency {e}")
+        return docker_compose_dependency.returncode
+
+    def spin_up_containers(self) -> int:
+        from main import FillOutComposeFile
+        compose_file_path = FillOutComposeFile.config_file_path
+        docker_compose_up = subprocess.run(['docker', 'compose', '-f', f'{compose_file_path}', 'up', '-d'], capture_output=True)
         if docker_compose_up.returncode != 0:
             error_message = print(f"couldn't spin up containers due to: {docker_compose_up.stderr}")
-            return Failure(error_description = error_message)
+            raise SystemExit(error_message)
         print('container/s are up')
-        return Success(return_value = docker_compose_up.returncode)
+        return docker_compose_up.returncode
 
-    def inspect_containers(self) -> Success[dict] | Failure:
+    def inspect_containers(self) -> dict:
         running_containers_ids: list = docker_client.containers.list(all = True, sparse=True)
         containers_config: dict[str, Any] = {}
         for container in running_containers_ids:
             containers_config[container.attrs["Names"][0].lstrip('/')] = container.attrs
-        return Success(return_value = containers_config)
+        return containers_config
 
-    def are_all_containers_up(self, selected_services, running_containers_names) -> Success | Failure:
+    def are_all_containers_up(self, selected_services, running_containers_names) -> bool:
         not_spun_up_services = [service for service in selected_services if service not in running_containers_names]
         if not not_spun_up_services:
-            return Success(return_value = True)
+            return True
         error_message = print(f"containers weren't created for the following services: {not_spun_up_services}")
-        return Failure(error_description = error_message)
+        return False
 
     @staticmethod
-    def check_container_state_param(container_attributes: dict) -> Success[bool] | Failure:
+    def check_container_state_param(container_attributes: dict) -> bool:
         if container_attributes["State"] != "running":
-            return Failure(error_description = "")
-        return Success(return_value = True)
+            return False
+        return True
 
-    def are_all_containers_healthy(self, running_containers: dict) -> Success[bool] | Failure:
+    def are_all_containers_healthy(self, running_containers: dict) -> bool:
         unhealthy_containers = [container for container in running_containers if not self.check_container_state_param(running_containers[container])]
         if not unhealthy_containers:
-            return Success(return_value = True)
+            return True
         error_message = print(f"the following containers are not healthy: {unhealthy_containers}")
-        return Failure(error_description = error_message)
+        return False
+
 
