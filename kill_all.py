a98d372adaacfef9925c4b4f2770b71c2787968a
VladyslavPodobed <vladyslav.podobed@gmail.com>
2026-09-08 07:06:06 +0300

    wrote script for getting rid of all containers

---
+++ kill_all.py
@@ -0,0 +1,29 @@
+import subprocess
+
+
+class ComposeDown:
+    def __init__(self) -> None:
+        pass
+
+    def observability_compose_down(self):
+        compose_down = subprocess.run(['docker', 'compose', 'down', '-v'], capture_output=True)
+
+    def services_compose_down(self):
+        compose_down = subprocess.run(['docker', 'compose', '-f', './generated/docker_compose.yml', 'down'], capture_output=True)
+
+
+class CheckIfDown:
+    def __init__(self) -> None:
+        pass
+
+    def is_observability_down(self):
+        pass
+
+
+def main():
+    compose_down = ComposeDown()
+    compose_down.observability_compose_down()
+    compose_down.services_compose_down()
+
+main()
+
