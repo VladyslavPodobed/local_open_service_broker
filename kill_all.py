506b2d4f3da55c55aadc560cd7472ac3b19e07c6
VladyslavPodobed <vladyslav.podobed@gmail.com>
2026-09-09 13:43:48 +0000

    added project name flag for the compose down command

--- kill_all.py
+++ kill_all.py
@@ -6,10 +6,10 @@ class ComposeDown:
         pass
 
     def observability_compose_down(self):
-        compose_down = subprocess.run(['docker', 'compose', 'down', '-v'], capture_output=True)
+        compose_down = subprocess.run(['docker', 'compose', '-p', 'generated', 'down', '-v'], capture_output=True)
 
     def services_compose_down(self):
-        compose_down = subprocess.run(['docker', 'compose', '-f', './generated/docker_compose.yml', 'down'], capture_output=True)
+        compose_down = subprocess.run(['docker', 'compose', '-p', 'generated', '-f', './generated/docker_compose.yml', 'down'], capture_output=True)
 
 
 class CheckIfDown:
