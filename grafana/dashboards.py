81edab6615c9e237ecc1f34c7e8e70edb0a4a389
VladyslavPodobed <vladyslav.podobed@gmail.com>
2026-09-14 00:15:02 +0000

    added a dashboard for host machine stats

--- grafana/dashboards.py
+++ grafana/dashboards.py
@@ -1,6 +1,6 @@
 from os import stat
 from grafana_foundation_sdk.builders.dashboard import Dashboard, Row
-from grafana_foundation_sdk.builders import prometheus, timeseries
+from grafana_foundation_sdk.builders import prometheus, timeseries, stat, gauge
 from grafana_foundation_sdk.cog.encoder import JSONEncoder
 from grafana_foundation_sdk.models.common import TimeZoneBrowser
 from grafana_foundation_sdk.models import units
@@ -10,15 +10,16 @@ def postgres_dashboard() -> Dashboard:
     builder = (
         Dashboard("Postgres")
         .uid("postgres")
-        .tags(["generated", "postgres"])
+        .tags(["postgres", "db"])
         .refresh("1m")
-        .time("now-30m", "now")
+        .time("now-60m", "now")
         .timezone(TimeZoneBrowser)
+
         .with_row(Row("availability"))
         .with_panel(
-            timeseries.Panel()
-            .title("postgres ip up/down")
-            .unit(units.BitsPerSecondSI)
+            stat.Panel()
+            .title("postgres is up/down")
+            .unit(units.NoUnit)
             .min(0)
             .with_target(
                 prometheus.Dataquery()
@@ -26,13 +27,56 @@ def postgres_dashboard() -> Dashboard:
                 .legend_format("{{ device }}")
             )
         )
-    )
 
+        .with_row(Row("storage"))
+        .with_panel(
+            timeseries.Panel()
+            .title("size of tables")
+            .unit(units.Terabytes)
+            .min(0)
+            .with_target(
+                prometheus.Dataquery()
+                .expr('pg_database_size_bytes')
+                .legend_format("{{ device }}")
+            )
+        )
+    )
+    return builder
+
+
+def host_machine_dashboard() -> Dashboard:
+    builder = (
+        Dashboard("Host machine info")
+        .uid("host_machine")
+        .tags(["host"])
+        .refresh("1m")
+        .time("now-60m", "now")
+        .timezone(TimeZoneBrowser)
+
+        .with_row(Row("host machine"))
+        .with_panel(
+            gauge.Panel()
+            .title("cpu load")
+            .unit(units.PercentUnit)
+            .min(0)
+            .max(100)
+            .with_target(
+                prometheus.Dataquery()
+                .expr('100 - (avg by (instance) (rate(node_cpu_seconds_total{mode="idle"}[5m])) * 100)')
+                .legend_format("{{ device }}")
+            )
+        )
+    )
     return builder
 
 
 postgres_dashboard = postgres_dashboard().build()
+host_machine_dashboard = host_machine_dashboard().build()
 encoder = JSONEncoder(sort_keys=True, indent=2)
 
 with open('./grafana/provisioning/dashboards/postgres_dashboad.json', 'w') as config_file:
     config_file.write(encoder.encode(postgres_dashboard))
+
+with open('./grafana/provisioning/dashboards/host_machine_dashboad.json', 'w') as config_file:
+    config_file.write(encoder.encode(host_machine_dashboard))
+
